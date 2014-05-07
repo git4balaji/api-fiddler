3d540ecb1b8b30497b7abecbe6195743eb4d59fd
Balaji Arun <ba2669@vt.edu>
2014-05-07 14:56:58 -0400

    Completed parser.

--- parser.py
+++ parser.py
@@ -1,32 +1,27 @@
-#! /usr/bin/end python3
-
-from object
+#! /usr/bin/env python3
 import yaml
 
-data = yaml.load( open('outline.yaml', 'r') )
-myobject = object(data)
-print(myobject)
-
-
-class objectParser:
+class ObjectParser:
     """ A object parser"""
-
-    def __init__(self,data)
-		self.objects = {}
-		for key,value in data.items():
-			self.objects[key] = Object(objects[key])
-
-    def __str__(self)
-		return "objectParser:\n{0}".format( "\n%%\n".join([name + ":" + str(object) for name,object in self.objects.items() ] ) ) 
-
+    
+    def __init__(self, data):
+        self.objects = {}
+        for key,value in data.items():
+            self.objects[key] = Object(value)
+    
+    def __str__(self):
+        return "objectParser:\n{0}".format( "\n%%\n".join([name + ":" + str(object) for name,object in self.objects.items() ] ) )
 
 class Object:
-	""" This is a  """
-	
-	def __init__(self,data):
-		self.items = []
-		for item in data:
-			self.item.append(item)
+    """ This is a  """
+    def __init__(self, data):
+        self.items = {}
+        for key,value in data.items():
+            self.items[key] = value
+    
+    def __str__(self):
+        return "Items:\n{0}".format( "\n%%\n".join([name + ":" + object for name,object in self.items.items() ] ) )
 
-
-	
+data = yaml.load( open('outline.yaml', 'r') )
+myobject = ObjectParser(data)
+print(myobject)
